65bfcf5ac43dfa26faee049a2c26eb691c2f4cde
essam salah <essamce@yahoo.com>
2021-02-27 16:22:34 +0200

    end_03

--- C3DAPITutorial/EntryPoint.cs
+++ C3DAPITutorial/EntryPoint.cs
@@ -14,71 +14,108 @@ using cApp = Autodesk.Civil.ApplicationServices;
 
 namespace C3DAPITutorial
 {
-    //accoremgd.dll
-    //acdbmgd.dll
-    //acmgd.dll
-    //AecBaseMgd.dll
-    //AeccDbMgd.dll
-     
     public class EntryPoint
     {
-        [CommandMethod("GetAlignments")]
-        public void GetAlignmetns()
+        [CommandMethod("CreateAlignmetByPolylineCmd")]
+        public void CreateAlignmetByPolylineCmd()
         {
             // get currunt document 
-            var aDocument = aApp.Application.DocumentManager.MdiActiveDocument;
-            var database = aDocument.Database;
-            var editor = aDocument.Editor;
-            var cDocument = cApp.CivilApplication.ActiveDocument;
+            var acDoc = aApp.Application.DocumentManager.MdiActiveDocument;
+            var database = acDoc.Database;
+            var editor = acDoc.Editor;
+            var civilDoc = cApp.CivilApplication.ActiveDocument;
 
-            // get alignmets from civil document
-            var allAlignmentsIds = cDocument.GetAlignmentIds();
-
-            // start transaction
-            using (var ts = database.TransactionManager.StartTransaction())
-            {
-                foreach (aDB.ObjectId id in allAlignmentsIds)
-                {
-                    var alignmet = id.GetObject(aDB.OpenMode.ForRead) as cDB.Alignment;
-                    editor.WriteMessage($"\nalignmet.Name: <{alignmet.Name,-15}>, alignmet.Length: <{alignmet.Length,-10}>");
-                }
-
-                ts.Commit();
-            }
-
-        }
-
-
-        [CommandMethod("GetAlignmetnBySelection")]
-        public void GetAlignmetnBySelection()
-        {
-            // get currunt document 
-            var aDocument = aApp.Application.DocumentManager.MdiActiveDocument;
-            var database = aDocument.Database;
-            var editor = aDocument.Editor;
-            var cDocument = cApp.CivilApplication.ActiveDocument;
-
-            // get alignmet by selection
-            var opt = new PromptEntityOptions("\nSelect an alignment");
-            opt.SetRejectMessage("\nObject must be an alignmet.");
-            opt.AddAllowedClass(typeof(cDB.Alignment), false);
+            // Ask the user to select a polyline to convert to an alignment
+            var opt = new PromptEntityOptions("\nSelect a polyline to convert to an Alignment");
+            opt.SetRejectMessage("\nObject must be a polyline.");
+            opt.AddAllowedClass(typeof(aDB.Polyline), false);
             var psr = editor.GetEntity(opt);
-
             if (psr.Status != PromptStatus.OK)
             {
                 editor.WriteMessage("\nNo Selection found");
                 return;
             }
 
-            // start transaction
-            using (var ts = database.TransactionManager.StartTransaction())
+            // create some polyline options for creating the new alignment
+            var polylineOptions = new cDB.PolylineOptions()
             {
-                var alignmet = psr.ObjectId.GetObject(aDB.OpenMode.ForRead) as cDB.Alignment;
-                editor.WriteMessage($"\nalignmet.Name: <{alignmet.Name,-15}>, alignmet.Length: <{alignmet.Length,-10}>");
+                AddCurvesBetweenTangents = true,
+                EraseExistingEntities = false,
+                PlineId = psr.ObjectId,
+            };
 
-                ts.Commit();
+            // create a new alignment
+            var testAlignmentID = cDB.Alignment.Create(civilDoc, polylineOptions, "New Alignment 1", "", "0", "Basic", "_No Labels");
+
+
+            //  the next line is identical to the previouse one, we just write it in declarative way
+            //var testAlignmentID = cDB.Alignment.Create(civilDoc,
+            //        plineOptions: polylineOptions,
+            //        alignmentName: "New Alignment 1",
+            //        siteName: "",
+            //        layerName: "0",
+            //        styleName: "Basic",
+            //        labelSetName: "_No Labels");
+        }
+
+
+        [CommandMethod("CreateAlignmetByPolylineCmd2")]
+        public void CreateAlignmetByPolylineCmd2()
+        {
+            // get currunt document 
+            var acDoc = aApp.Application.DocumentManager.MdiActiveDocument;
+            var database = acDoc.Database;
+            var editor = acDoc.Editor;
+            var civilDoc = cApp.CivilApplication.ActiveDocument;
+
+            // Ask the user to select a polyline to convert to an alignment
+            var opt = new PromptEntityOptions("\nSelect a polyline to convert to an Alignment");
+            opt.SetRejectMessage("\nObject must be a polyline.");
+            opt.AddAllowedClass(typeof(aDB.Polyline), false);
+            var psr = editor.GetEntity(opt);
+            if (psr.Status != PromptStatus.OK)
+            {
+                editor.WriteMessage("\nNo Selection found");
+                return;
             }
 
+            // create some polyline options for creating the new alignment
+            var polylineOptions = new cDB.PolylineOptions()
+            {
+                AddCurvesBetweenTangents = true,
+                EraseExistingEntities = false,
+                PlineId = psr.ObjectId,
+            };
+
+            // get layer "0" id
+            var layerId = database.LayerZero;
+
+            // get an AlignmentStyle id and check if the drawings has no AlignmentStyles.
+            aDB.ObjectId alignmentStyleId;
+            if (civilDoc.Styles.AlignmentStyles.Count > 0)
+            {
+                alignmentStyleId = civilDoc.Styles.AlignmentStyles[0];
+            }
+            else
+            {
+                editor.WriteMessage("\n No Alignments styles found");
+                return;
+            }
+
+            // get an AlignmentLabelSetStyle and check if the drawings has no AlignmentLabelSetStyles.
+            aDB.ObjectId alignmentLablesSetId;
+            if (civilDoc.Styles.LabelSetStyles.AlignmentLabelSetStyles.Count > 0)
+            {
+                alignmentLablesSetId = civilDoc.Styles.LabelSetStyles.AlignmentLabelSetStyles[0];
+            }
+            else
+            {
+                editor.WriteMessage("\n No Alignments label set found");
+                return;
+            }
+
+            // create a new alignment
+            var testAlignmentID = cDB.Alignment.Create(civilDoc, polylineOptions, "New Alignment 11", aDB.ObjectId.Null, layerId, alignmentStyleId, alignmentLablesSetId);
         }
 
     }
